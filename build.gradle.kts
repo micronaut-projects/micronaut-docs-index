77588956168084c26818ddbf637811f29090f0c4
Sergio del Amo <sergio.delamo@softamo.com>
2022-07-05 18:26:44 +0200

    local assets

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,7 @@
 apply<io.micronaut.website.gradle.MicronautWebsiteDocsIndexPlugin>()
+
+tasks.register<Copy>("copyAssets") {
+    from(layout.projectDirectory.dir("assets"))
+    into(layout.buildDirectory.dir("assets"))
+}
+tasks.findByName("build")?.dependsOn("copyAssets")
